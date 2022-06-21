2a57186428e7e406a7da13415c82521fe4e79f1e
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-06-21 20:41:34 -0300

    agregando paginacion

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -6,6 +6,8 @@ const NoticiasContext = createContext()
 const NoticiasProvider = ({children}) => {
     const [categoria, setCategoria] = useState('general')
     const [noticias, setNoticias] = useState([])
+    const [pagina, setPagina] = useState(1)
+    const [totalNoticias, setTotalNoticias] = useState(0)
 
 
     useEffect(() => {
@@ -13,6 +15,7 @@ const NoticiasProvider = ({children}) => {
             const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
             const { data } = await axios(url)
             setNoticias(data.articles)
+            setTotalNoticias(data.totalResults)
         }
         consultarAPI()
     }, [categoria])
