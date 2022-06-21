e200ec1288688f7621345d6eaee09811a3afabac
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-06-21 11:50:55 -0300

    Form + context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import Formulario from './components/Formulario';
+import {Container, Grid, Typography} from '@mui/material';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 
 function App() {
   
 
   return (
-   <h1>App Noticias</h1>
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align='center' marginY={5} component="h1" variant='h3'>
+            Buscador de Noticias
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent='center'
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+
+      </Container>
+   </NoticiasProvider>
   )
 }
 
